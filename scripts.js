b07cbfd5bc442c71cfb74dd82068ff13a733d72d
BillyBob554 <imabot2925@gmail.com>
2023-02-01 15:35:43 +0000

    Completed Project

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function getStarted() {
+    window.open("https://the-redirector.senorita.workers.dev/poopy")
+    play()
+}
+
+function play() {
+    var audio = new Audio('/audio/poop.mp3');
+    audio.play();
+  }
